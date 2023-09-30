2b271fed0db7fdae275aebd62c8a00295d51848d
MartínRGC <martingarcia08.00@gmail.com>
2023-09-30 08:41:45 -0500

    se creó el repository Orders

---
+++ UESAN.Store.INFRASTRUCTURE/Repositories/OrdersRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UESAN.Store.INFRASTRUCTURE.Repositories
+{
+    internal class OrdersRepository
+    {
+    }
+}
